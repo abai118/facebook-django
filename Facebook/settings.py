a59d4c60e630ba2667c63773f2a82e32bebecbe7
akhil118 <gandikota.abai@gmail.com>
2022-07-14 17:12:38 +0530

    home page

--- Facebook/settings.py
+++ Facebook/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'Facebook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'facebook',
-        'USER':  'postgres',
-        'PASSWORD' :  'root',
+        'NAME': config('DB_NAME'),
+        'USER':  config('DB_USER'),
+        'PASSWORD' : config('DB_PASSWORD'),
         'HOST' : config('DB_HOST'),
-        'listen_addresses' : '*',
-        'OPTIONS': {
-                'client_encoding': 'UTF8',
-            }
 
     }
 }
